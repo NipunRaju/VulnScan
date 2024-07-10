03e7d6edf1a92780ec862285a2651d4593e2343e
NipunRaju <nipunraju2008@gmail.com>
2024-07-11 02:35:36 +0530

    added port

--- main.py
+++ main.py
@@ -83,5 +83,5 @@ def scan():
     return render_template('index.html', output=output)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
